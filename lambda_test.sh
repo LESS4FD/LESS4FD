06ddc205e33a8d3a7a0227ad04d7fd9c5983d95c
LESS4FD <164395520+LESS4FD@users.noreply.github.com>
2024-04-16 12:33:39 +0100

    Update lambda_test.sh

--- lambda_test.sh
+++ lambda_test.sh
@@ -2,14 +2,6 @@
 
 for l_ce in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
 
-python main.py --lambda_ce $l_ce --lambda_cr 0.5
-
-done
-
-## Test on Lambda_cr
-
-for l_cr in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1; do
-
-python main.py --lambda_cr $l_cr --lambda_ce 0.5
+python main.py --lambda_ce $l_ce 
 
 done
